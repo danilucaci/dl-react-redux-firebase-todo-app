2e9b62307a20f268d270298235e2ac7c598b90b9
Dani Lucaci <danilucaci@gmail.com>
2019-10-25 21:15:12 +0200

    Modify useFocusRef hook to return a ref callback

--- src/hooks/useFocusRef.js
+++ src/hooks/useFocusRef.js
@@ -1,13 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useCallback, useState } from "react";
 
 // @use
-// useFocusRef(ref);
-function useFocusRef(ref) {
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.focus();
+// const firstItemRef = useFocusRef();
+function useFocusRef() {
+  const [nodeRef, setNodeRef] = useState(null);
+  // https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
+  //
+  // Using a callback ref ensures that even if a child component displays
+  // the measured node later (e.g. in response to a click),
+  // we still get notified about it in the parent component and can update the measurements.
+  // Note that we pass [] as a dependency array to useCallback.
+  // This ensures that our ref callback doesn’t change between the re-renders,
+  // and so React won’t call it unnecessarily.
+  const nodeRefCb = useCallback((refMounted) => {
+    if (refMounted) {
+      setNodeRef(refMounted);
     }
-  }, [ref]);
+  }, []);
+
+  useEffect(() => {
+    if (nodeRef) {
+      nodeRef.focus();
+    }
+  }, [nodeRef]);
+
+  return nodeRefCb;
 }
 
 export default useFocusRef;
