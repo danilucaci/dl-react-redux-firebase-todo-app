34b6b06f9caeed15e3d33f4ad0d4ec52432eac05
Dani Lucaci <danilucaci@gmail.com>
2019-10-31 16:16:39 +0100

    Add reselect tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,13 @@ import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./root-reducer";
 
+import * as ReselectTools from "reselect-tools";
+import * as colorsSelectors from "./colors/colors-selectors";
+import * as projectsSelectors from "./projects/projects-selectors";
+import * as labelsSelectors from "./labels/labels-selectors";
+import * as todosSelectors from "./todos/todos-selectors";
+import * as localStateSelectors from "./localState/localState-selectors";
+
 const middlewares = [];
 
 if (process.env.NODE_ENV === "development") {
@@ -13,5 +20,17 @@ export const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middlewares)),
 );
+
+ReselectTools.registerSelectors(
+  todosSelectors,
+  colorsSelectors,
+  projectsSelectors,
+  labelsSelectors,
+  localStateSelectors,
+);
+
+ReselectTools.selectorGraph();
+
+ReselectTools.getStateWith(() => store.getState());
 
 export default store;
