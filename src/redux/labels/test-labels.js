1e86468c038fbf48b9150c01b5990f6f0895113c
Dani Lucaci <danilucaci@gmail.com>
2019-11-06 14:28:33 +0100

    Set the initial state in reducers as an empty object

---
+++ src/redux/labels/test-labels.js
@@ -0,0 +1,37 @@
+const testLabels = {
+  "fb1d5a40-b853-434d-a3e3-0f19eb4e9430": {
+    id: "fb1d5a40-b853-434d-a3e3-0f19eb4e9430",
+    uid: "d0382da9-85cd-4787-b4d3-1dbb9c35e607",
+    name: "pending",
+    todosCount: 1,
+    color: {
+      colorID: "050211ec-448b-48d8-be67-b78222318c0a",
+      colorName: "Orange",
+      colorValue: "#F19D4B",
+    },
+  },
+  "d0fd61ff-87e0-4c36-9386-dada2cdadb56": {
+    id: "d0fd61ff-87e0-4c36-9386-dada2cdadb56",
+    uid: "d0382da9-85cd-4787-b4d3-1dbb9c35e607",
+    name: "soon",
+    todosCount: 3,
+    color: {
+      colorID: "1fb29f8d-7d81-4daf-9b3d-3dfbce5e4384",
+      colorName: "Teal",
+      colorValue: "#428DAA",
+    },
+  },
+  "e56b81a2-af3b-4364-8dbb-e1ed6e5edeec": {
+    id: "e56b81a2-af3b-4364-8dbb-e1ed6e5edeec",
+    uid: "d0382da9-85cd-4787-b4d3-1dbb9c35e607",
+    name: "relax",
+    todosCount: 0,
+    color: {
+      colorID: "f05c3cc2-6e2e-449c-a0ef-b36563370ec7",
+      colorName: "Green",
+      colorValue: "#4B9144",
+    },
+  },
+};
+
+export default testLabels;
