93defcf0221077f1daa841dcc336a5df4d9135fb
Dani Lucaci <danilucaci@gmail.com>
2019-11-06 10:53:25 +0100

    Remove colors parent object in colors reducer

--- src/redux/colors/colors-selectors.js
+++ src/redux/colors/colors-selectors.js
@@ -1,6 +1,6 @@
 import { createSelector } from "reselect";
 
-export const selectColors = (state) => Object.values(state.colors.colors.byID);
+export const selectColors = (state) => Object.values(state.colors.byID);
 
 export const colorsSelector = createSelector(
   [selectColors],
