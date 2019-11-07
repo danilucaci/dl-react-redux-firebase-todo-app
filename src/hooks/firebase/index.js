2886c19db852c57c18a01769592c2d25a075da25
Dani Lucaci <danilucaci@gmail.com>
2019-11-07 15:59:11 +0100

    Add firebase hooks

---
+++ src/hooks/firebase/index.js
@@ -0,0 +1,1 @@
+export { default as useCollection } from "./useCollection";
---
+++ src/hooks/firebase/useCollection.js
@@ -0,0 +1,67 @@
+import { useRef, useEffect, useReducer } from "react";
+
+import getDocsObject from "../../utils/firebase/getDocsObject";
+
+export const collectionState = {
+  loading: true,
+  error: null,
+  data: null,
+};
+
+export const collectionReducer = (state, action) => {
+  switch (action.type) {
+    case "save": {
+      return {
+        ...state,
+        data: getDocsObject(action.payload),
+        loading: false,
+      };
+    }
+    case "error": {
+      return {
+        ...state,
+        error: true,
+        loading: false,
+        data: null,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+/**
+ * Subscribes to a forestore collection.
+ * @param {firestore.Query} query The firestore query to subscribe to.
+ * @returns {?Object} Error returned from firestore.
+ * @returns {boolean} If the data is still loading.
+ * @returns {?Object} The data fetched from firestore.
+ */
+function useCollection(query) {
+  const collectionRef = useRef(query);
+  const [
+    { error = null, loading = false, data = {} } = {},
+    dispatch,
+  ] = useReducer(collectionReducer, collectionState);
+
+  useEffect(() => {
+    if (!collectionRef.current) return;
+
+    const unsubscribe = collectionRef.current.onSnapshot(
+      function handleSnapshot(snapshot) {
+        return dispatch({ type: "save", payload: snapshot.docs });
+      },
+      function handleError(error) {
+        return dispatch({ type: "error", payload: error });
+      },
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return [error, loading, data];
+}
+
+export default useCollection;
