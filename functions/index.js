9cef62fcf43c7b9bc39232d27f8e011fc85a267e
Dani Lucaci <danilucaci@gmail.com>
2019-11-14 10:43:46 +0100

    Add working example firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firestore = admin.firestore();
+
+exports.getAllTodos = functions.https.onRequest(async (request, response) => {
+  const snapshot = await firestore.collection("colors").get();
+
+  const colors = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+
+  response.json({ colors });
+});
